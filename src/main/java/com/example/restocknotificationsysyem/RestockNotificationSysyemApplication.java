59f8f30605a8f297db9375daec16c0b66552c15e
Hwiyoung Jeon <wjsgnldud@naver.com>
2024-12-14 11:36:29 +0900

    first commit

---
+++ src/main/java/com/example/restocknotificationsysyem/RestockNotificationSysyemApplication.java
@@ -0,0 +1,13 @@
+package com.example.restocknotificationsysyem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestockNotificationSysyemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestockNotificationSysyemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restocknotificationsysyem/RestockNotificationSysyemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restocknotificationsysyem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestockNotificationSysyemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
